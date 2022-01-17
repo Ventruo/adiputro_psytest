43eee835cf85fff556a02e0d4767ae731c44fa28
ventruo <lawrencepatrick0@gmail.com>
2022-01-17 17:14:43 +0700

    Fix Express url configuration

--- backend/src/setup/express.js
+++ backend/src/setup/express.js
@@ -1,37 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-const PORT = process.env.EXPRESS_PORT || 8080;
+const PORT = process.env.EXPRESS_PORT || 3000;
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// let url;
+// if (process.env.CLIENT_PORT) {
+//   url = process.env.CLIENT_URL || "http://127.0.0.1:5050";
+// } else {
+//   url = process.env.EXPRESS_URL || "http://127.0.0.1:3000";
+// }
 
-let url;
-if (process.env.CLIENT_PORT) {
-    url =
-        (process.env.PROTOCOL || 'http') +
-        '://' +
-        (process.env.CLIENT_URL || '127.0.0.1') +
-        ':' +
-        (process.env.CLIENT_PORT || 5050);
-} else {
-    url = (process.env.PROTOCOL || 'http') + '://' + process.env.CLIENT_URL;
-}
-
-app.use(
-    cors({
-        origin: [url, 'http://localhost:5050'],
-        credentials: true,
-    })
-);
+// app.use(
+//   cors({
+//     origin: [url, "http://localhost:5050"],
+//     credentials: true,
+//   })
+// );
 
 const initExpress = () => {
-    app.listen(PORT, () => {
-        console.log(`Server started on ${url}`);
-    });
-}
-
+  app.listen(PORT, () => {
+    console.log(`Server started on ${process.env.EXPRESS_URL}`);
+  });
+};
 
 module.exports = { app, initExpress };
