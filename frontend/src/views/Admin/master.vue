f0524a00461d65a12b5962701bdd680fbfaa97c6
WideanNagari <Wideannagari@gmail.com>
2022-06-03 23:40:27 +0700

    fix bug tampilan

--- frontend/src/views/Admin/master.vue
+++ frontend/src/views/Admin/master.vue
@@ -86,7 +86,7 @@
                 <p >Hi, Admin</p>
                 <p >{{timestamp||""}}</p>
             </div>
-            <div class="overflow-auto no-scrollbar h-screen w-full relative mt-2">
+            <div class="overflow-auto h-screen w-full relative mt-2">
                 <!-- <div class="overflow-hidden absolute w-full h-72 -z-10">
                     <svg viewBox="0 0 500 150" preserveAspectRatio="none" class="h-full w-full">
                         <path d="M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z" class="fill-foreground-4-50"></path>
