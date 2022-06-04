6710502b90f04cf372dc98f9982e66d4fb6df75d
ventruo <lawrencepatrick0@gmail.com>
2022-06-04 17:03:39 +0700

    Re-enable Socket

--- frontend/src/views/normalTest.vue
+++ frontend/src/views/normalTest.vue
@@ -577,41 +577,41 @@ export default {
             // console.log(this.test_result_id)
         })
 
-        // this.getTempAnswers();
+        this.getTempAnswers();
 
-        // // Build socket
-        // const access_token = localStorage.getItem('LS_ACCESS_KEY_VAR').split(' ')[1]
-        // const user_key = localStorage.getItem('LS_USER_KEY_VAR')
-        // // console.log(access_token);
-        // // console.log(user_key)
-        // buildSocket(access_token, user_key).then((socket) => {
-        //     socket.on("test.tick", (data) => {
-        //         if(data.section_id == this.section_id){
-        //             // console.log("socket", socket)
-        //             // console.log(data);
-        //             this.isStarted = true
-        //             this.duarsi = data.total_duration;
-        //             var minutes = Math.floor(data.countdown / 60);
-        //             var seconds = data.countdown - minutes * 60;
+        // Build socket
+        const access_token = localStorage.getItem('LS_ACCESS_KEY_VAR').split(' ')[1]
+        const user_key = localStorage.getItem('LS_USER_KEY_VAR')
+        // console.log(access_token);
+        // console.log(user_key)
+        buildSocket(access_token, user_key).then((socket) => {
+            socket.on("test.tick", (data) => {
+                if(data.section_id == this.section_id){
+                    // console.log("socket", socket)
+                    // console.log(data);
+                    this.isStarted = true
+                    this.duarsi = data.total_duration;
+                    var minutes = Math.floor(data.countdown / 60);
+                    var seconds = data.countdown - minutes * 60;
 
-        //             this.menit = (new Array(2+1).join('0')+minutes).slice(-2);
-        //             this.detik = (new Array(2+1).join('0')+seconds).slice(-2);
-        //             if(data.countdown <= 0){
-        //                 Swal.fire({
-        //                     title: 'Waktu Habis...',
-        //                     icon: 'warning',
-        //                     confirmButtonColor: '#3085d6',
-        //                     confirmButtonText: 'Kembali ke Dashboard',
-        //                     allowOutsideClick: false,
-        //                 }).then((result) => {
-        //                     if (result.isConfirmed) {
-        //                         this.submitJawaban()
-        //                     }
-        //                 });
-        //             }
-        //         }
-        //     });
-        // });
+                    this.menit = (new Array(2+1).join('0')+minutes).slice(-2);
+                    this.detik = (new Array(2+1).join('0')+seconds).slice(-2);
+                    if(data.countdown <= 0){
+                        Swal.fire({
+                            title: 'Waktu Habis...',
+                            icon: 'warning',
+                            confirmButtonColor: '#3085d6',
+                            confirmButtonText: 'Kembali ke Dashboard',
+                            allowOutsideClick: false,
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                this.submitJawaban()
+                            }
+                        });
+                    }
+                }
+            });
+        });
 
         let thi = this
         $('body').keydown(function(event) {
